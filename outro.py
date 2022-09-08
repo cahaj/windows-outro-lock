377484269c624bcb41e62dcffd5eff03549593dc
Jiri <63856816+cahaj@users.noreply.github.com>
2022-09-08 16:14:43 +0200

    Create outro.py

---
+++ outro.py
@@ -0,0 +1,27 @@
+import threading
+import ctypes
+from playsound import playsound
+import time
+
+def play():
+    playsound('TheFatRat-XeogenesisOutroPart.mp3')
+
+def countandlock():
+    time.sleep(2)
+    print("5")
+    time.sleep(3)
+    print("4")
+    time.sleep(3)
+    print("3")
+    time.sleep(3)
+    print("2")
+    time.sleep(3)
+    print("1")
+    time.sleep(2)
+    ctypes.windll.user32.LockWorkStation()
+
+
+x = threading.Thread(target=play)
+x.start()
+x = threading.Thread(target=countandlock)
+x.start()
