97c687496d030c7ccd5137329f8d1a3cbf598f42
Jiri <63856816+cahaj@users.noreply.github.com>
2022-09-10 11:17:56 +0200

    Update outro.py

--- outro.py
+++ outro.py
@@ -2,9 +2,16 @@ import threading
 import ctypes
 from playsound import playsound
 import time
+import os
+import sys
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
 
 def play():
-    playsound('TheFatRat-XeogenesisOutroPart.mp3')
+    playsound(resource_path('TheFatRat-XeogenesisOutroPart.mp3'))
 
 def countandlock():
     time.sleep(2)
